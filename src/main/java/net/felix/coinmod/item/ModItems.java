86141c6deef453c2a612d9ae27c33d75c6e5bc01
Felix-Galle <felixjohngalle@gmail.com>
2024-12-28 06:14:05 +0100

    Adding the coin_fragment, without errors

--- src/main/java/net/felix/coinmod/item/ModItems.java
+++ src/main/java/net/felix/coinmod/item/ModItems.java
@@ -1,7 +1,7 @@
 package net.felix.coinmod.item;
 
 
-import net.fabricmc.fabric.api.item.v1.FabricItem;
+
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.felix.coinmod.CoinMod;
@@ -12,14 +12,14 @@ import net.minecraft.registry.Registries;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
-    public static final Item COIN_FRAGMENT = registerItem("coin_fragment", new Item(new Fabric));
+    public static final Item COIN_FRAGMENT = registerItem("coin_fragment", new Item(new Item.Settings()));
 
     private static void addItemsToIngredientTabItemGroup(FabricItemGroupEntries entries) {
         entries.add(COIN_FRAGMENT);
     }
 
     private static Item registerItem(String name, Item item) {
-        return Registry.register(Registries.ITEM, new Identifier(CoinMod.MOD_ID, name), item);
+        return Registry.register(Registries.ITEM, Identifier.of(CoinMod.MOD_ID, name), item);
     }
 
     public static void registerModItems() {
