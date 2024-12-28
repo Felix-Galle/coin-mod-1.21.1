706973de9c08415337ec59ad1ca17f780d87f3cf
Felix-Galle <felixjohngalle@gmail.com>
2024-12-28 05:46:41 +0100

    Initial commit

---
+++ src/main/java/net/felix/coinmod/CoinModClient.java
@@ -0,0 +1,10 @@
+package net.felix.coinmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CoinModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
